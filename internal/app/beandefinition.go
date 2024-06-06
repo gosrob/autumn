2259aab7230e311fa32f440f432db6768d823170
Mrzrb <zhangruobin@zuoyebang.com>
2024-06-06 20:44:30 +0800

    Add bean definition and types for the application

--- internal/app/beandefinition.go
+++ internal/app/beandefinition.go
@@ -1,3 +1,38 @@
 package app
 
-type BeanDefinition struct{}
+type BeanClass string
+
+// BeanDefinition represents the definition of a bean in the application.
+type BeanDefinition struct {
+	// IsBuiltinType indicates whether the type is a built-in Go type.
+	IsBuiltinType bool
+
+	// # beanClass is the fully qualified name of the bean's class.
+	//
+	// particularly {full-packageName}.{typeName}
+	// TODO: current not use this field
+	BeanClass BeanClass
+
+	// TODO: Currently, this field is not used.
+	DependsOn []string
+
+	// IsPrimary indicates if the bean is the primary candidate for autowiring.
+	// TODO: Currently, this field is not used.
+	IsPrimary bool
+
+	// IsLazy specifies whether the bean should be lazily initialized.
+	// TODO: Currently, this field is not used.
+	IsLazy bool
+
+	// Consturcts is a map where each key is a string representing the name of a bean,
+	// and each value is a Constructor function that creates an instance of that bean.
+	// TODO: Currently, this field is not used.
+	Consturcts map[string]Constructor
+
+	Bean StructDefinition
+}
+
+type Constructor struct {
+	ConstructBeanName string
+	ConstructorFn     func() string
+}
---
+++ internal/app/types.go
@@ -0,0 +1,41 @@
+package app
+
+// # represents a struct
+//
+// this struct contains struct field
+type StructDefinition struct {
+	Name string
+
+	// struct field
+	Fields []Field
+}
+
+// # represent a struct field
+type Field struct {
+	// Field name
+	Name string
+
+	// # Field Type
+	//
+	// which is full package type, if one field is in another package and is a pointer, type is *github.com/xxx/xxx/pkg.{TypeName}.
+	//
+	// if field type is basic type, it just act like a normal type.
+	Type string
+}
+
+// FuncDefinition represents the structure of a function definition, including its name,
+// parameters, and results.
+type FuncDefinition struct {
+	Name    string  // Name is the name of the function.
+	Params  []Param // Params is a slice of parameters the function accepts.
+	Results []Param // Results is a slice of parameters the function returns.
+}
+
+// Param represents a parameter within a function definition.
+type Param struct {
+	Name string // Name is the name of the parameter.
+
+	Type string // Type is the type of the parameter. It can be a full package type
+	// (e.g., *github.com/xxx/xxx/pkg.{TypeName} for a pointer type in another package)
+	// or a basic type.
+}
