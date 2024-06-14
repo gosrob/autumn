aed986dc77dcec7ede3d73b5d26303c89fd937ac
Mrzrb <zhangruobin@zuoyebang.com>
2024-06-14 17:37:01 +0800

    finish inject

--- main.go
+++ main.go
@@ -1,23 +1,17 @@
 package main
 
 import (
-	"fmt"
-
 	_ "github.com/YReshetko/go-annotation/annotations/constructor"
 	_ "github.com/YReshetko/go-annotation/annotations/mapper"
 	_ "github.com/YReshetko/go-annotation/annotations/validator"
+	annotation "github.com/YReshetko/go-annotation/pkg"
 	"github.com/gosrob/autumn/internal/logger"
 	_ "github.com/gosrob/autumn/internal/processor"
-	"github.com/gosrob/autumn/pkg/container"
-	"github.com/gosrob/autumn/testdata/beandefinition/subdirectory"
 	_ "github.com/gosrob/autumn/wire"
-	"github.com/samber/do/v2"
 )
 
 func main() {
 	defer logger.Logger.CatchPanic()
-	a := do.MustInvoke[subdirectory.B](container.Container)
-	fmt.Println(a)
-	// logger.Logger.SetIsDebug(true)
-	// annotation.Process()
+	logger.Logger.SetIsDebug(true)
+	annotation.Process()
 }
